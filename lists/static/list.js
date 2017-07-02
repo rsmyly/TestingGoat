2105032b14b9e57d419440995b9805f4a87ccb3e
Robert Smyly <robert@smyly.net>
2017-07-02 16:02:09 -0500

    clear errors when input box is clicked

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,10 @@
-var initialize = function() {
+window.Superlists = {};
+window.Superlists.initialize = function() {
 	$('input[name="text"]').on('keypress', function() {
 		$('.has-error').hide();
 	});
+	$('input[name="text"]').on('click', function() {
+		$('.has-error').hide();
+	});
+
 };
