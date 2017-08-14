6094e283388357931efbfbb963944d565a2c5950
Robert Smyly <robert@smyly.net>
2017-08-13 20:45:31 -0500

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -9,49 +9,41 @@ SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
 
-    def test_can_get_email_link_to_log_in(self):
-        #Edit goes to the awesome superlists site
-        #and notices a "Log in" section in the navbar for the first time
-        #It's telling her to enter her email address, so she does
-        self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
-        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+	def test_can_get_email_link_to_log_in(self):
+		#Edit goes to the awesome superlists site
+		#and notices a "Log in" section in the navbar for the first time
+		#It's telling her to enter her email address, so she does
+		self.browser.get(self.live_server_url)
+		self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+		self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
-        #A message appears telling her an email has been sent
-        self.wait_for(lambda: self.assertIn(
-            'Check your email',
-            self.browser.find_element_by_tag_name('body').text
-        ))
+		#A message appears telling her an email has been sent
+		self.wait_for(lambda: self.assertIn(
+			'Check your email',
+			self.browser.find_element_by_tag_name('body').text
+		))
 
-        #She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+		#She checks her email and finds a message
+		email = mail.outbox[0]
+		self.assertIn(TEST_EMAIL, email.to)
+		self.assertEqual(email.subject, SUBJECT)
 
-        #It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
-        if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
-        url = url_search.group(0)
-        self.assertIn(self.live_server_url, url)
+		#It has a url link in it
+		self.assertIn('Use this link to log in', email.body)
+		url_search = re.search(r'http://.+/.+$', email.body)
+		if not url_search:
+			self.fail(f'Could not find url in email body:\n{email.body}')
+		url = url_search.group(0)
+		self.assertIn(self.live_server_url, url)
 
-        #she clicks it
-        self.browser.get(url)
+		#she clicks it
+		self.browser.get(url)
 
-        #she is logged in
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+		#she is logged in
+		self.wait_to_be_logged_in(email=TEST_EMAIL)
 
-        #now she logs out
-        self.browser.find_element_by_link_text('Log out').click()
+		#now she logs out
+		self.browser.find_element_by_link_text('Log out').click()
 
-        #She is logged out
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+		#She is logged out
+		self.wait_to_be_logged_out(email=TEST_EMAIL)
